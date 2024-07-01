4cad75cd770733c7cebe2cb4d56f35a92ec78daa
Nikolas Burk <nikolas.burk@gmail.com>
2024-07-01 12:23:40 +0200

    store results in file

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,35 @@
-import writeResults from './lib/write-results';
-import prepare from './lib/prepare';
-import { prismaPg }  from './prisma-postgres'
-import { typeormPg }  from './typeorm-postgres'
-import { drizzlePg } from './drizzle-postgres';
+import writeResults from "./lib/write-results";
+import prepare from "./lib/prepare";
+import { prismaPg } from "./prisma-postgres";
+import { typeormPg } from "./typeorm-postgres";
+import { drizzlePg } from "./drizzle-postgres";
+import * as fs from 'fs';
 
 async function runBenchmarks() {
-  await prepare()
-  console.log(`db ready`)
-  const prismaResults = await prismaPg()
+  await prepare();
+  console.log(`db ready`);
+  const prismaResults = await prismaPg();
 
-  await prepare()
-  const drizzleResults = await drizzlePg()
+  await prepare();
+  const drizzleResults = await drizzlePg();
 
-  await prepare()
-  const typeormResults = await typeormPg()
+  await prepare();
+  const typeormResults = await typeormPg();
 
-  writeResults('prisma', prismaResults)
-  writeResults('drizzle', drizzleResults)
-  writeResults('typeorm', typeormResults)
+  const arrays: any = { prisma: prismaResults, drizzle: drizzleResults, typeorm: typeormResults };
+
+  const header = [""].concat(prismaResults.map((item) => item.query));
+  const rows = [];
+
+  for (const key in arrays) {
+    const row = [key].concat(arrays[key].map((item: { time: any; }) => item.time));
+    rows.push(row);
+  }
+
+  const csvContent = [header, ...rows].map((row) => row.join(",")).join("\n");
+  console.log(csvContent)
+
+  fs.writeFileSync(`./results/${new Date()}.csv`, csvContent);
 }
 
-runBenchmarks()
+runBenchmarks();
