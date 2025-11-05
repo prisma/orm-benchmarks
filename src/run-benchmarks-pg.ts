22d5304a3c457614a8189d3d6418ea47eb7fbbef
Ankur Datta <64993082+ankur-arch@users.noreply.github.com>
2025-11-06 04:55:10 +0600

    feat: alt migration

--- src/run-benchmarks-pg.ts
+++ src/run-benchmarks-pg.ts
@@ -1,5 +1,5 @@
-import { preparePg } from "./lib/prepare-pg-native"; // seed via `pg_restore`
-// import { preparePg } from "./lib/prepare-pg-prisma"; // seed via `createMany`
+//import { preparePg } from "./lib/prepare-pg-native"; // seed via `pg_restore`
+import { preparePg } from "./lib/prepare-pg-prisma"; // seed via `createMany`
 import writeResults from "./lib/write-results";
 import { BenchmarkOptions, MultipleBenchmarkRunResults } from "./lib/types";
 import { prismaPg } from "./prisma/prisma-pg";
